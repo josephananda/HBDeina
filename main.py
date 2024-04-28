e62dad356b2e44b39218602980c3d5a151951764
joseph <josephananda88888@gmail.com>
2024-04-28 13:17:34 +0700

    Safe run

--- main.py
+++ main.py
@@ -7,43 +7,6 @@ import streamlit as st
 #from PIL import Image
 #import numpy as np
 
-'''
-def draw_flower():
-    speed(0)
-    bgcolor('black')
-    h = 0
-    frames = []
-    for i in range(16):
-        for j in range(18):
-            c = colorsys.hsv_to_rgb(h, 1, 1)
-            color(c)
-            h += 0.005
-            rt(90)
-            circle(150 - j * 6, 90)
-            lt(90)
-            circle(150 - j * 6, 90)
-            rt(180)
-            frames.append(get_frame_as_np_array())
-        circle(40, 24)
-    #done()
-    print(len(frames))
-    with imageio.get_writer("flower_animation.mp4", mode="I") as writer:
-        for index, frame in enumerate(frames):
-            print("Index:", index)
-            # Do something with the frame
-            writer.append_data(frame)
-
-
-# Function to convert Turtle canvas to numpy array
-def get_frame_as_np_array():
-    canvas = getscreen().getcanvas()
-    canvas.postscript(file="temp.eps", colormode="color")
-    img = Image.open("temp.eps")
-    img_np = np.array(img)
-    return img_np
-'''
-
-
 def main():
     #draw_flower()
     titles = st.title("Happy Birthday Deina")
